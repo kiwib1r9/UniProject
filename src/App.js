7e811fb484a7a6828236ca92c2c5c073ff6f6811
clara <kiwib1r9@gmail.com>
2024-05-22 11:04:31 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+      <Container customClass="min_height">
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/contact" element={<Contact />}/>
+          <Route exact path="/company" element={<Company />}/>
+          <Route exact path="/newproject" element={<NewProject />}/>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
+
+  )
 }
 
-export default App;
+export default App
