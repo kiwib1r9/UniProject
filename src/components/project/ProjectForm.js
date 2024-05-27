2904bd4778292eaebf3a0a7b8dabcd93a8e53351
clara <kiwib1r9@gmail.com>
2024-05-27 14:25:57 -0300

    iniciando API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,31 @@
 import styles from './ProjectForm.module.css'
+
 import Input from './Input'
 import Select from './Select'
 import SubmitBtn from './SubmitBtn'
 
+import {useEffect, useState} from 'react'
+
 function ProjectForm({btnText}){
+    const [categories, setCategories] = useState([])
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories',{
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {setCategories(data)})
+            .catch((err) => console.log(err))        
+    }, [])
+
     return(
         <form className={styles.form}> 
             <Input type='text' text='Nome do projeto' name='name' placeholder='Digite o nome do seu projeto' />
             <Input type='number' text='Prazo do projeto' name='time' placeholder='Insira o prazo em dias do seu projeto'/>
-            <Select name='category_id' text='Selecione a categoria' />
+            <Select options={categories} name='category_id' text='Selecione a categoria' />
             <SubmitBtn text={btnText}/>
         </form>
     )
