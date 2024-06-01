044b29c3d5181f8b162301f593df1f033f87b61c
clara <kiwib1r9@gmail.com>
2024-06-01 11:21:38 -0300

    projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,8 @@
 import styles from './Projects.module.css'
 
+import Container from '../layout/Container'
 import Message from '../layout/Message'
+import LinkButton from '../layout/LinkButton'
 
 import { useLocation } from 'react-router-dom'
 
@@ -13,9 +15,15 @@ function Projects(){
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>          
+            </div>
             {message && <Message msg={message} type='success'/>}
+            <Container customClass='start'>
+                <p>Projetos</p>
+            </Container>
         </div>
     )
 }
